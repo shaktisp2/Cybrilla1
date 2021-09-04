fb9ffd33394dfd1f63ff9444d20ecd10665ca05e
shaktisp2 <90083077+shaktisp2@users.noreply.github.com>
2021-09-04 12:05:27 +0530

    Update problem3.java
    Added some changes.

--- problem3.java
+++ problem3.java
@@ -4,15 +4,16 @@ import java.util.Scanner;
 
 public class Problem1 {
 
-
+// Checking for palindrome eg. "abcba", "sdffds", etc
+	
 	static boolean isPalindrome(String str)
 	{
 
-		int i = 0, j = str.length() - 1;
+		int i = 0, j = str.length() - 1;  // To start from leftmost and rightmost corner in string
 
 		while (i < j) {
 
-			if (str.charAt(i) != str.charAt(j))
+			if (str.charAt(i) != str.charAt(j))    // comparing characters while they are same
 				return false;
 
 			i++;
